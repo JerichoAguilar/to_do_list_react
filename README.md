1b1c608bc793af9359c3857e0797e548d6f0c351
JerichoAguilar <43047356+JerichoAguilar@users.noreply.github.com>
2021-12-04 08:43:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Firebase Integration
+
+## You will need to create a new repo for this class  
+## call it something like firebase-vercel
+
+# ENV FILES
+
+## CONFIG FILE
+
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import firebase from '../firebase';
+
+
+export default function Home() {
+   console.log(firebase);
+  return (
+     <div>
+       
+     </div>
+     
+       
+    
+  )
+}
