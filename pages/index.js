bdbd9b05a19d7e7194e4e1f1c06bf0a1c87c3aa9
JerichoAguilar <43047356+JerichoAguilar@users.noreply.github.com>
2021-12-04 09:23:31 -0700

    added buttons

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import firebase from '../firebase';
-
+import Button from '../components/ui/button';
 
 export default function Home() {
   return (
      <div>
-       
+       <Button>Login With Google</Button>
+       <Button>Login With Email</Button>
      </div>
      
        
